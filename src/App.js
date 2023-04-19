a9e4b66e4d4e51089643ef902bc4af9c53005151
Mafe <mafxrnandxs@gmail.com>
2023-04-19 19:05:00 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [form, setForm] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const [isUserRegistered, setIsUserRegistered] = useState(false); // novo estado
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form); // envie os dados do formulário para um servidor aqui
+    alert('Usuário cadastrado!');; // atualize o estado para indicar que o usuário foi cadastrado com sucesso
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <form onSubmit={handleSubmit}>
+      <h1>Inscreva-se</h1>
+      <label>
+        Nome:
+        <input type="text" name="firstName" value={form.firstName} onChange={handleChange} />
+      </label>
+      <label>
+        Sobrenome:
+        <input type="text" name="lastName" value={form.lastName} onChange={handleChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" value={form.email} onChange={handleChange} />
+      </label>
+      <label>
+        Senha:
+        <input type="password" name="password" value={form.password} onChange={handleChange} />
+      </label>
+      <button type="submit">Enviar</button>
+      {isUserRegistered && <p>Usuário cadastrado com sucesso!</p>}
+    </form>
   );
 }
 
